2f81bacd0e839f4fb62c4c2e3c6977adec90121a
Francisco Pezoa <49153826+Panshios@users.noreply.github.com>
2020-03-11 12:55:37 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,12 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 600);
   colorMode(HSB,360,100,100)
-  background(10,90,30);
+  background(10,90,30);  
 }
 
 function draw() {
-  var gam = mouseX/10*random(1,4);
+  var gam = 20*random(1,4);
   fill(random(170,235),20,100);
   noStroke();
-  ellipse(mouseX,mouseY,gam,gam);
-  
+  ellipse(mouseX,mouseY,gam+movedX,gam+movedX);
 }
