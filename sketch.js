e918e31f4a20cf9b715a31d950520733d2eed2a4
Francisco Pezoa <49153826+Panshios@users.noreply.github.com>
2020-03-11 11:31:57 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(600, 600);
+  colorMode(HSB,360,100,100)
+  background(10,90,30);
+}
+
+function draw() {
+  var gam = mouseX/10*random(1,4);
+  fill(random(170,235),20,100);
+  noStroke();
+  ellipse(mouseX,mouseY,gam,gam);
+  
+}
